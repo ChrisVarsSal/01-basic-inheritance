6d4e3efc44cbdd1813074d13b52cfba079908ef4
Maikol Guzman Alan <mguzmana@akamai.com>
2020-07-26 21:56:16 -0600

    Several changes
    Signed-off-by: Maikol Guzman Alan <mguzmana@akamai.com>

--- BasicInheritance.cpp
+++ BasicInheritance.cpp
@@ -1,5 +1,46 @@
-//
-// Created by Maikol Guzman on 7/26/20.
-//
+/*
+ * =====================================================================================
+ *
+ *       Filename:  example_01.cpp
+ *
+ *    Description:  Basic of inheritance
+ *
+ *        Created:  26/07/2020
+ *
+ *         Author:  Maikol Guzmán mikeguzman@gmail.com
+ *   Organization:  Universidad Nacional de Costa Rica
+ *
+ * =====================================================================================
+ */
 
-#include "BasicInheritance.h"
+#include <iostream>  // allows program to output data to the screen
+
+struct Base {
+    Base(const int number) : memberNumber(number) {
+    }
+    void doSomething() {
+        memberNumber += 2;
+    }
+
+    int memberNumber = 0;
+};
+
+struct Derived : Base {
+    Derived() : Base(5) { }
+    void doSomethingElse() {
+        ++memberNumber;
+    }
+};
+
+// function main begins program execution
+int main(int argc, const char *argv[]) {
+    std::cout << "Welcome to the UNA!" << std::endl;  // display message
+
+    Derived derived;
+    derived.doSomething();
+    derived.doSomethingElse();
+
+    std::cout << derived.memberNumber << '\n'; // What is printed?
+
+    return 0;  // indicate that program ended successfully
+}  // end function main
